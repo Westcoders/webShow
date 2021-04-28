5951f9cb1ba84e2a04a0fe166038a6ca7f0e6b8f
头头 <646257251@qq.com>
2021-04-28 16:31:09 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,12 +21,3 @@ createApp(App).mount('#app')
     }
 
   }).mount('#aaa')
-/*
-.js内容学习笔记
-一. 创建组件
-1. const RootComponent = { 应用的功能 }:写算法；
-2. const app = Vue.createApp(RootComponent)：登记某个应用到"app"（自定义名字）中；
-3. const vm = app.mount('#app'):将app这个应用挂载在ID为“app”中，便于与dom使用，???其中vm作用未知???；
-4. 1与2可合并使用：const app = vue.createApp({应用的功能})，随后直接挂载；
-二.模板
-1.*/
