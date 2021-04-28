0606276e7512b3d7ee25bea9f08595694c5e8b4f
头头 <646257251@qq.com>
2021-04-28 16:43:28 +0800

    Update TestBox.vue

--- src/components/TestBox.vue
+++ src/components/TestBox.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="testBox">
-    <fast-test></fast-test>
     <br />
     <img-check-group
       :groupValues="groupValuesOne"
@@ -8,41 +7,22 @@
       :defaultCheckeds="groupCheckeds"
     ></img-check-group>
     <br />
-    <button-group>
+    <*--<button-group>
       <button-option size="small"></button-option>
       <button-option name="点我" :isAllowed="false"></button-option>
       <button-option size="Middle" icon="search"></button-option>
     </button-group>
-    <br />
-    <grid-row>
-      <grid-col></grid-col>
-      <grid-col></grid-col>
-      <grid-col></grid-col>
-      <grid-col></grid-col>
-    </grid-row>
-    <!--<card-play></card-play>-->
+    <br />-->
   </div>
 </template>
 
 <script>
 import ImgCheckGroup from "./ImgCheckGroup.vue";
-import ButtonOption from "./ButtonOption.vue";
-import ButtonGroup from "./ButtonGroup.vue";
-import GridRow from "./GridRow.vue";
-import GridCol from "./GridCol.vue";
-import FastTest from "./FastTest.vue";
-import cardPlay from "./cardPlay.vue";
 export default {
   name: "TestBox",
   components: {
     ImgCheckGroup,
-    ButtonOption,
-    ButtonGroup,
-    GridRow,
-    GridCol,
-    FastTest,
-    cardPlay,
-  },
+   },
   data() {
     return {
       groupValuesOne: ["图一", "图二", "图三", "图四"],
